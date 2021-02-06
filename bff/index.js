d15e74e92af44002b3ff19745e47a84b8a38b6d9
Alexis Donato <alexis.donato95@gmail.com>
2021-02-06 18:47:24 -0300

    Se habilita el CORS para desarrollo

--- bff/index.js
+++ bff/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const config = require('./config');
 const items = require('./apis/routes/items');
 const notFoundMiddleware = require('./middlewares/notFound')
 const logger = require('./middlewares/logger');
 
-app.use(logger);
+app.use(cors());
 
+app.use(logger);
 app.use(config.prefix.base, items);
 app.use(notFoundMiddleware);
 
